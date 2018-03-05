d74f03097fbcd2817b68be04050727f5668cd24b
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-03-05 00:44:46 -0600

    added post, put, and delete http requests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // instantitate the eastHTTP object
 const http = new easyHTTP;
 
-// Get posts
+// GET request (all)
 // use get prototype function
 http.get('http://jsonplaceholder.typicode.com/posts', function(err, posts){
    if (err) {
@@ -10,3 +10,50 @@ http.get('http://jsonplaceholder.typicode.com/posts', function(err, posts){
       console.log(posts);
    }
 });
+
+// GET request (single post)
+http.get('http://jsonplaceholder.typicode.com/posts/1', function(err, post){
+   if (err) {
+      console.log(err);
+   } else {
+      console.log(post);
+   }
+});
+
+// POST request
+// create data
+const data = {
+   title: 'Custom Post',
+   body: 'This is a custom post'
+};
+
+// Create post
+// 1st parameter: API url
+// 2nd parameter: data to send
+// 3rd parameter: callback function
+http.post('http://jsonplaceholder.typicode.com/posts', data, function(err, post){
+   if (err) {
+      console.log(err);
+   } else {
+      console.log(post);
+   }
+});
+
+// PUT request - update post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err,post){
+   if (err) {
+      console.log(err);
+   } else {
+      console.log(post);
+   }
+});
+
+// DELETE request
+http.delete('http://jsonplaceholder.typicode.com/posts/1', function(err, response){
+   if (err) {
+      console.log(err);
+   } else {
+      console.log(response);
+   }
+});
+
--- easyhttp.js
+++ easyhttp.js
@@ -17,12 +17,54 @@ easyHTTP.prototype.get = function(url, callback){
          callback(`Error: ${self.http.status}`);
       }
    }
-
    this.http.send();
 }
 
 // HTTP POST Request
+easyHTTP.prototype.post = function (url, data, callback) {
+   this.http.open('POST', url, true);
+
+   // set the content type, which is in the HTTP header
+   // 1st parameter: the option to set. In this case, the option is 'Content-type' 
+   // 2nd parameter: what 'Content-type' is set to. In this case, content-type is set to a value of application/json, which is the data type that is being used. 
+   this.http.setRequestHeader('Content-type', 'application/json');
+
+   let self = this;
+   this.http.onload = function() {
+      // the callback takes in null for an error if there is one. 
+      callback(null, self.http.responseText);
+      // responseText should be the new POST that was added. It should have an id of 101 because the last item id in the GET request responseText was 100.
+   }
+   
+   // This contains the data that is being posted 
+   // 'data' is a JS object when it is passed into this.send(). It needs to be stringified, which converts it to JSON. 
+   this.http.send(JSON.stringify(data));
+}
 
 // HTTP PUT Request
+easyHTTP.prototype.put = function (url, data, callback) {
+   this.http.open('PUT', url, true);
+   this.http.setRequestHeader('Content-type', 'application/json');
+
+   let self = this;
+   this.http.onload = function() {
+      callback(null, self.http.responseText);
+      
+   }
+   this.http.send(JSON.stringify(data));
+}
 
 // HTTP DELETE Request
+easyHTTP.prototype.delete = function(url, callback){
+   this.http.open('DELETE', url, true);
+
+   let self = this;
+   this.http.onload = function() {
+      if(self.http.status === 200) { 
+         callback(null, 'Post Deleted');
+      } else { // if the http.status is NOT 200
+         callback(`Error: ${self.http.status}`);
+      }
+   }
+   this.http.send();
+}
